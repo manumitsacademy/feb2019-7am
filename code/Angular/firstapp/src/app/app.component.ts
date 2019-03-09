ae8fb9bd4f768d43b0b6f459e5896885b6b8fbb5
manumits.info <manumits.info@gmail.com>
2019-03-09 08:07:02 +0530

    cards

--- code/Angular/firstapp/src/app/app.component.ts
+++ code/Angular/firstapp/src/app/app.component.ts
@@ -6,20 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  //properties
-  firstname = "praveen";
-  lastname = 'gubbala';
-  location = 'pune';
-  tasks=[];
-  //methods
-  getFullName(){
-    console.log(this.firstname,this.lastname)
-  }
-  add(firstnumber,secondnumber){
-    console.log(parseInt(firstnumber)+parseInt(secondnumber))
-  }
-  addTask(t){
-    this.tasks.push(t);
-    console.log(this.tasks);
-  }
+  
 }
